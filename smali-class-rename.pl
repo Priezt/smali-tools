ddb6399836ef053c21bd3ebd2590ca5077b1c918
Priezt <vegetabird@gmail.com>
2014-12-15 22:30:48 +0800

    add check before rename class

--- smali-class-rename.pl
+++ smali-class-rename.pl
@@ -7,4 +7,5 @@ use Data::Dumper;
 
 $old_class = shift @ARGV or die "need old class name";
 $new_class = shift @ARGV or die "need new class name";
+die "new classname wrong" if $new_class =~ /\(/;
 Smali::rename_class($old_class, $new_class);
